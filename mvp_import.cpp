84a02ff8af72daa4332e1247182ddab335b36b4e
yx500 <yx500@mail.ru>
2020-04-15 15:04:36 +0300

    111

---
+++ mvp_import.cpp
@@ -0,0 +1,6 @@
+#include "mvp_import.h"
+
+MVP_Import::MVP_Import()
+{
+
+}
---
+++ mvp_import.h
@@ -0,0 +1,11 @@
+#ifndef MVP_IMPORT_H
+#define MVP_IMPORT_H
+
+
+class MVP_Import
+{
+public:
+    MVP_Import();
+};
+
+#endif // MVP_IMPORT_H
